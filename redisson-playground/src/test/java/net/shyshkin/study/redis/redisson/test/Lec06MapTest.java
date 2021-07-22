7d1cccf9ee854a9a4a7dfe7de844cb0e8f6a2337
Art <d.art.shishkin@gmail.com>
2021-07-22 21:54:38 +0300

    50. Map - Part 2 (#2)

--- redisson-playground/src/test/java/net/shyshkin/study/redis/redisson/test/Lec06MapTest.java
+++ redisson-playground/src/test/java/net/shyshkin/study/redis/redisson/test/Lec06MapTest.java
@@ -1,14 +1,22 @@
 package net.shyshkin.study.redis.redisson.test;
 
 import lombok.extern.slf4j.Slf4j;
+import net.shyshkin.study.redis.redisson.test.dto.Student;
 import org.junit.jupiter.api.Test;
 import org.redisson.api.RMapReactive;
 import org.redisson.client.codec.StringCodec;
+import org.redisson.codec.TypedJsonJacksonCodec;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 public class Lec06MapTest extends BaseTest {
@@ -50,5 +58,45 @@ public class Lec06MapTest extends BaseTest {
         StepVerifier
                 .create(putAll)
                 .verifyComplete();
+    }
+
+    @Test
+    void mapTest_objectSave() {
+
+        //given
+        TypedJsonJacksonCodec codec = new TypedJsonJacksonCodec(Integer.class, Student.class);
+        RMapReactive<Integer, Student> map = client.getMap("users", codec);
+
+        //when
+        Map<Integer, Student> studentMap = IntStream
+                .rangeClosed(1, 3)
+                .boxed()
+                .collect(Collectors.toMap(Function.identity(), i -> generateRandomStudent()));
+
+        Mono<Void> putAll = map.putAll(studentMap);
+
+        Mono<Map<Integer, Student>> getAll = map.getAll(Set.of(1, 2, 3, 4, 5, 6, 7));
+
+        //then
+        StepVerifier
+                .create(putAll.then(getAll))
+                .assertNext(
+                        resultMap -> assertThat(resultMap)
+                                .satisfies(resMap -> log.info("Retrieved map: {}", resMap))
+                                .hasSize(3)
+                                .allSatisfy((idx, student) -> assertThat(student).hasNoNullFieldsOrProperties())
+                )
+                .verifyComplete();
+    }
+
+    private Student generateRandomStudent() {
+        return Student.builder()
+                .name(FAKER.name().firstName())
+                .city(FAKER.address().city())
+                .age(FAKER.random().nextInt(18, 45))
+                .mark(FAKER.random().nextInt(100))
+                .mark(FAKER.random().nextInt(100))
+                .mark(FAKER.random().nextInt(100))
+                .build();
     }
 }
