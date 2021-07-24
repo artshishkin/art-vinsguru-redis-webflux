7dc6e09f2a3c2cd6a9616675e45ca3bd508260ea
Art <d.art.shishkin@gmail.com>
2021-07-24 15:00:20 +0300

    56. Queue & Stack (#2)

--- redisson-playground/src/test/java/net/shyshkin/study/redis/redisson/test/Lec09ListQueueStackTest.java
+++ redisson-playground/src/test/java/net/shyshkin/study/redis/redisson/test/Lec09ListQueueStackTest.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RDequeReactive;
 import org.redisson.api.RListReactive;
+import org.redisson.api.RQueueReactive;
 import org.redisson.client.RedisException;
 import org.redisson.client.codec.LongCodec;
 import reactor.core.publisher.Flux;
@@ -149,6 +151,100 @@ public class Lec09ListQueueStackTest extends BaseTest {
                             .isInstanceOf(RedisException.class)
                             .hasMessageContaining("index: 666 but current size: 0")
                     );
+        }
+    }
+
+    @Nested
+    class QueueTest {
+
+        private RQueueReactive<Long> queue;
+
+        @BeforeEach
+        void setUp() {
+            queue = client.getQueue("number-input", LongCodec.INSTANCE);
+        }
+
+        @Test
+        void queueTest() {
+
+            //given
+            int itemsToPoll = 4;
+
+            //when
+            Flux<Long> queuePoll = queue
+                    .poll()
+                    .repeat(itemsToPoll - 1)
+                    .doOnNext(item -> log.info("Item polled: {}", item));
+
+            //then
+            StepVerifier
+                    .create(queuePoll)
+                    .expectNextCount(itemsToPoll)
+                    .verifyComplete();
+            StepVerifier
+                    .create(queue.size())
+                    .expectNext(10 - itemsToPoll)
+                    .verifyComplete();
+        }
+    }
+
+    @Nested
+    class StackTest {
+
+        private RDequeReactive<Long> deque;
+
+        @BeforeEach
+        void setUp() {
+
+            initList();
+            deque = client.getDeque("number-input", LongCodec.INSTANCE);
+        }
+
+        private void initList() {
+            Flux<Long> listAdd = Flux.range(1, 10)
+                    .map(Long::valueOf);
+
+            RListReactive<Long> list = client
+                    .getList("number-input", LongCodec.INSTANCE);
+
+            Mono<Boolean> deleteList = list
+                    .delete()
+                    .doOnNext(res -> log.info("Delete: {}", res));
+
+            StepVerifier.create(deleteList)
+                    .expectNextCount(1)
+                    .verifyComplete();
+
+            Mono<Boolean> addAll = list
+                    .addAll(listAdd)
+                    .doOnNext(res -> log.info("Added: {}", res));
+
+            StepVerifier.create(addAll)
+                    .expectNextCount(1)
+                    .verifyComplete();
+        }
+
+        @Test
+        void queueTest() {
+
+            //given
+            int itemsToPoll = 4;
+
+            //when
+            Flux<Long> stackPoll = deque
+                    .pollLast()
+                    .repeat(itemsToPoll - 1)
+                    .doOnNext(item -> log.info("Item polled: {}", item));
+
+            //then
+            StepVerifier
+                    .create(stackPoll)
+                    .expectNext(10L, 9L, 8L, 7L)
+                    .verifyComplete();
+            StepVerifier
+                    .create(deque.size())
+                    .expectNext(10 - itemsToPoll)
+                    .verifyComplete();
         }
     }
 }
