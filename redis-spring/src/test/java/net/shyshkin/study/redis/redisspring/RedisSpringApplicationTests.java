ce8643d385e639b4b9f6231dd81740ec29ff6254
Art <d.art.shishkin@gmail.com>
2021-07-26 11:57:14 +0300

    70.1. Spring Data Redis vs Redisson - Default Spring Data (#3)

--- redis-spring/src/test/java/net/shyshkin/study/redis/redisspring/RedisSpringApplicationTests.java
+++ redis-spring/src/test/java/net/shyshkin/study/redis/redisspring/RedisSpringApplicationTests.java
@@ -1,13 +1,57 @@
 package net.shyshkin.study.redis.redisspring;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.data.redis.core.ReactiveValueOperations;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
+@Slf4j
 @SpringBootTest
 class RedisSpringApplicationTests {
 
+    @Autowired
+    ReactiveStringRedisTemplate template;
+
     @Test
-    void contextLoads() {
+    void valueOperationsTest() {
+        ReactiveValueOperations<String, String> valueOperations = template.opsForValue();
+
+        Mono<Boolean> set = valueOperations.set("user:1:name", "Art");
+        Mono<String> get = valueOperations.get("user:1:name");
+
+        Mono<String> pipeline = set.then(get).doOnNext(name -> log.debug("Retrieved name: {}", name));
+
+        StepVerifier.create(pipeline)
+                .expectNext("Art")
+                .verifyComplete();
+    }
+
+    @RepeatedTest(3)
+    void manyNetworkCallsTest() {
+
+        long before = System.currentTimeMillis();
+
+        ReactiveValueOperations<String, String> valueOperations = template.opsForValue();
+        Mono<Void> insertion = Flux.range(1, 500_000)
+                .flatMap(i -> valueOperations.increment("user:1:visits")) //incr
+                .then();
+
+        Duration duration = StepVerifier.create(insertion)
+                .verifyComplete();
+
+        long after = System.currentTimeMillis();
+
+        log.debug("Duration: {}", duration);
+        log.debug("Time taken: {} ms", after - before);
+
     }
 
 }
