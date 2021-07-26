c89eb34f9ed846e41a72243057e1ba805c43cacb
Art <d.art.shishkin@gmail.com>
2021-07-26 11:32:23 +0300

    68. Introduction (#3 Section 5: Spring WebFlux Caching)

---
+++ redis-spring/src/test/java/net/shyshkin/study/redis/redisspring/RedisSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.redis.redisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
