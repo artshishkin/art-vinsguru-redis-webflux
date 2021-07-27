a1e58f93f3f0bdae5e517e97256087e5b2efce51
Art <d.art.shishkin@gmail.com>
2021-07-27 15:31:06 +0300

    85. Setting TTL (#3)

--- redis-spring/src/main/java/net/shyshkin/study/redis/redisspring/city/service/CityService.java
+++ redis-spring/src/main/java/net/shyshkin/study/redis/redisspring/city/service/CityService.java
@@ -2,20 +2,29 @@ package net.shyshkin.study.redis.redisspring.city.service;
 
 import net.shyshkin.study.redis.redisspring.city.client.CityClient;
 import net.shyshkin.study.redis.redisspring.city.dto.City;
-import org.redisson.api.RMapReactive;
+import org.redisson.api.RMapCacheReactive;
 import org.redisson.api.RedissonReactiveClient;
 import org.redisson.codec.TypedJsonJacksonCodec;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CityService {
 
-    private final RMapReactive<String, City> cityMap;
+    private final RMapCacheReactive<String, City> cityMap;
     private final CityClient cityClient;
 
+    @Value("${app.city.info.ttl.value}")
+    private long ttlValue;
+
+    @Value("${app.city.info.ttl.unit}")
+    private TimeUnit ttlUnit;
+
     public CityService(RedissonReactiveClient redissonClient, CityClient cityClient) {
-        cityMap = redissonClient.getMap("city:info", new TypedJsonJacksonCodec(String.class, City.class));
+        cityMap = redissonClient.getMapCache("city:info", new TypedJsonJacksonCodec(String.class, City.class));
         this.cityClient = cityClient;
     }
 
@@ -29,7 +38,7 @@ public class CityService {
         return cityClient
                 .getCityInfo(zipcode)
                 .flatMap(city -> cityMap
-                        .fastPut(zipcode, city)
+                        .fastPut(zipcode, city, ttlValue, ttlUnit)
                         .thenReturn(city));
     }
 }
--- redis-spring/src/test/java/net/shyshkin/study/redis/redisspring/city/service/CityServiceTest.java
+++ redis-spring/src/test/java/net/shyshkin/study/redis/redisspring/city/service/CityServiceTest.java
@@ -5,6 +5,7 @@ import net.shyshkin.study.redis.redisspring.city.dto.City;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -14,22 +15,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @SpringBootTest
+@TestPropertySource(
+        properties = {
+                "app.city.info.ttl.value=2"
+        }
+)
 class CityServiceTest {
 
     @Autowired
     CityService cityService;
 
     @Test
-    void getCityInfo() {
+    void getCityInfo() throws InterruptedException {
         //given
         String zipcode = "00603";
 
         //when
+        Duration duration = externalServiceCall(zipcode);
+        log.debug("First call of external service took {}", duration);
+
+        //then
+        duration = externalServiceCall(zipcode);
+
+        log.debug("Second call of external service took {} (retrieve cached value)", duration);
+        assertThat(duration).isLessThan(Duration.ofMillis(200));
+
+        //wait expiration
+        Thread.sleep(2100);
+        duration = externalServiceCall(zipcode);
+
+        log.debug("Third call of external service took {} (after cache has been expired)", duration);
+        assertThat(duration).isGreaterThan(Duration.ofSeconds(1));
+
+        //wait for redisson__timeout__set:{city:info} in Redis server to be cleared by Redisson
+        Thread.sleep(5000);
+    }
+
+    private Duration externalServiceCall(String zipcode) {
         Mono<City> cityInfo = cityService
                 .getCityInfo(zipcode)
                 .doOnNext(city -> log.debug("Retrieved city from external service: {}", city));
 
-        Duration duration = StepVerifier.create(cityInfo)
+        return StepVerifier.create(cityInfo)
                 .thenConsumeWhile(
                         city -> true,
                         city -> assertThat(city)
@@ -39,22 +66,5 @@ class CityServiceTest {
                                 .hasFieldOrPropertyWithValue("stateName", "Puerto Rico")
                 )
                 .verifyComplete();
-
-        log.debug("First call of external service took {}", duration);
-
-        //then
-        duration = StepVerifier.create(cityInfo)
-                .thenConsumeWhile(
-                        city -> true,
-                        city -> assertThat(city)
-                                .hasNoNullFieldsOrProperties()
-                                .hasFieldOrPropertyWithValue("zipcode", zipcode)
-                                .hasFieldOrPropertyWithValue("city", "Aguadilla")
-                                .hasFieldOrPropertyWithValue("stateName", "Puerto Rico")
-                )
-                .verifyComplete();
-
-        log.debug("Second call of external service took {}", duration);
-        assertThat(duration).isLessThan(Duration.ofMillis(200));
     }
 }
