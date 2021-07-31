e5e0e8893a9d49579b7cd44ea4a07fa0ef59c7bb
Art <d.art.shishkin@gmail.com>
2021-07-31 11:21:46 +0300

    114. Chat Messages Broadcasting (#8)

--- redis-spring/src/main/java/net/shyshkin/study/redis/redisspring/chat/service/ChatRoomService.java
+++ redis-spring/src/main/java/net/shyshkin/study/redis/redisspring/chat/service/ChatRoomService.java
@@ -1,22 +1,47 @@
 package net.shyshkin.study.redis.redisspring.chat.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RTopicReactive;
+import org.redisson.api.RedissonReactiveClient;
+import org.redisson.client.codec.StringCodec;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class ChatRoomService implements WebSocketHandler {
+
+    private final RedissonReactiveClient redissonClient;
 
     @Override
     public Mono<Void> handle(WebSocketSession webSocketSession) {
 
+        String room = "dummy";
+
+        RTopicReactive topic = this.redissonClient.getTopic(room, StringCodec.INSTANCE);
+
         //subscribe
-        webSocketSession.receive();
+        Mono<Void> subscribe = webSocketSession.receive()
+                .map(WebSocketMessage::getPayloadAsText)
+                .flatMap(topic::publish)
+                .doOnError(thr -> log.error("Error", thr))
+                .doFinally(st -> log.debug("Subscriber finally: {}", st))
+                .then();
 
         //publisher
-        webSocketSession.send();
+        Flux<WebSocketMessage> publisher = topic.getMessages(String.class)
+                .map(webSocketSession::textMessage)
+                .doOnError(thr -> log.error("Error", thr))
+                .doFinally(st -> log.debug("Publisher finally: {}", st));
 
-        return null;
+        Mono<Void> send = webSocketSession.send(publisher);
+
+        return subscribe.then(send);
     }
 }
